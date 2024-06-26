efc47a6be96b2739e3a585ea0aa06caa2f4b075d
Mário Balen <mariobalen3@gmail.com>
2024-06-25 21:34:13 -0300

    criada funcao do desenho do balao

---
+++ balao.py
@@ -0,0 +1,15 @@
+import pygame
+def draw_speech_bublle(screen, text, text_colour, bg_colour, pos, size):
+    font = pg.font.SysFont(None, size)
+    text_surface = font.render(text, True, text_colour)
+    text_rect = text_surface.get_rect(midbottom=pos)
+
+    bg_rect = text_rect.copy()
+    bg_rect.inflate_ip(10,10)
+
+    frame_rect = bg_rect.copy()
+    frame_rect.inflate_ip(4,4)
+
+    pg.draw.rect(screen, text_colour, frame_rect)
+    pg.draw.rect(screen, bg_colour, bg_rect)
+    screem.bilit(text_surface, text_rect)
