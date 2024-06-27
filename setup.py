3eaf854fc5437b02c245450fe392ed3f30ee71da
Pablo <pablosarturi1@gmail.com>
2024-06-26 21:49:56 -0300

    setup ajustado

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # pip install cx_freeze
 import cx_Freeze
 executaveis = [ 
-               cx_Freeze.Executable(script="main.py", icon="assets/icone.ico") ]
+               cx_Freeze.Executable(script="main.py", icon="recursos/icone.ico") ]
 cx_Freeze.setup(
-    name = "Iron Man",
+    name = "Olivia the Destroyer",
     options={
         "build_exe":{
             "packages":["pygame"],
-            "include_files":["assets"]
+            "include_files":["recursos"]
         }
     }, executables = executaveis
 )
